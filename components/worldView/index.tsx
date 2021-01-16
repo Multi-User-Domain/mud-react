1945215e4c6b39afb2df18f4b7bafd4e5204395e
Calum Mackervoy <c.mackervoy@gmail.com>
2021-01-16 12:20:15 +0000

    worldView component defines table

--- components/worldView/index.tsx
+++ components/worldView/index.tsx
@@ -1,13 +1,35 @@
 import {
+    Table,
+    TableColumn,
+} from "@inrupt/solid-ui-react";
+
+import {
   Container,
   Box
 } from "@material-ui/core";
 
+import { RDF, VCARD, FOAF } from "@inrupt/lit-generated-vocab-common";
+import { MUD } from "../../lib/MUD";
+
+import useMudWorld from '../../lib/hooks/useMudWorld';
+
 export default function WorldView(): React.ReactElement {
+    const { settlementDataSet, settlements } = useMudWorld();
+
+    if (!settlementDataSet || !settlements) return <div>loading...</div>;
+
+    const settlementThings = settlements.map((thing) => ({
+      dataset: settlementDataSet,
+      thing: thing,
+    }));
+
+    // TODO: write a generic table component for Characters & Settlements to share
     return (
         <Container fixed>
             <Box>
-                <h1>Hello, World!</h1>
+                <Table things={settlementThings}>
+                    <TableColumn property={VCARD.fn} header="Name" />
+                </Table>
             </Box>
         </Container>
     );
