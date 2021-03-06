f044d0156c549683e9cf79b7c389cd3a70491b06
Calum Mackervoy <c.mackervoy@gmail.com>
2021-03-06 21:43:35 +0000

    ActionManager

--- lib/MUD.tsx
+++ lib/MUD.tsx
@@ -2,6 +2,7 @@
 
 const MUD_BASE_URL = "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mud.ttl";
 const MUD_CONTENT_BASE_URL = "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudcontent.ttl";
+const MUD_LOGIC_BASE_URL = "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudlogic.ttl";
 
 export const MUD = {
     account: MUD_BASE_URL + '#Account',
@@ -23,9 +24,14 @@ export const MUD_CONTENT = {
     sight: MUD_CONTENT_BASE_URL + "#sight",
 }
 
+export const MUD_LOGIC = {
+    Transit: MUD_LOGIC_BASE_URL + "#Transit",
+}
+
 // TODO: https://github.com/calummackervoy/mud-react/issues/4
 export const MUDAPI = {
     settlementsPath: 'mud/settlements/',
     
-    contentPath: 'mud/content/'
+    contentPath: 'mud/content/',
+    taskPath: 'mud/act/task/',
 }
