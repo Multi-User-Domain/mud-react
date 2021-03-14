cff6f6c4aeb7718a7d716d8a85676f6365811881
Calum Mackervoy <c.mackervoy@gmail.com>
2021-03-14 16:27:59 +0000

    displays character task status in Character component

--- components/character/index.tsx
+++ components/character/index.tsx
@@ -1,14 +1,18 @@
 import { Grid, GridItem, Center, Text } from "@chakra-ui/react";
 import { getThingName } from '../../lib/utils';
 import ThingDepiction from "../thingDepiction";
-import { Thing } from '@inrupt/solid-client';
+import { getUrl } from '@inrupt/solid-client';
 import { IRowComponent } from "../thingList";
+import { MUD_CHARACTER } from "../../lib/MUD";
 import styles from "./character.module.css";
 
 export default function Character({thing, selectHandler} : IRowComponent): React.ReactElement {  
   const onCharacterSelect = (event) => {
     selectHandler(thing);
   }
+
+  const task = getUrl(thing, MUD_CHARACTER.hasTask);
+  const taskComponent = task ? <Text color="#e34e1b" fontWeight="bold">BUSY</Text> : <Text color="#77a513" fontWeight="bold">IDLE</Text>;
   
   return (
   <Grid templateColumns="repeat(5, 1fr)" w="100%" gap={1} className={styles.characterRow}>
@@ -22,8 +26,8 @@ export default function Character({thing, selectHandler} : IRowComponent): React
       <Center h="100%"><Text>{getThingName(thing)}</Text></Center>
     </GridItem>
     
-    <GridItem w="100%" colSpan={1} colEnd={6} bg="blue.500" className={styles.characterField}>
-      <Center h="100%"><Text>Buttons</Text></Center>
+    <GridItem w="100%" colSpan={1} colEnd={6} className={styles.characterField}>
+      <Center h="100%">{taskComponent}</Center>
     </GridItem>
   </Grid>);
   }
