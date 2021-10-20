04ea87f45cc2cc1c0d533e1425c9a9bacee8aab1
Calum Mackervoy <c.mackervoy@gmail.com>
2021-10-20 20:44:32 +0200

    GameWindow only displays characters for now

--- components/gameWindow/index.tsx
+++ components/gameWindow/index.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React from "react";
 
 import { 
     Tabs,
@@ -7,29 +7,17 @@ import {
     Tab, 
     TabPanel} from "@chakra-ui/react";
 
-import useTerminalFeed from "../../lib/hooks/useTerminalFeed";
-import styles from "./terminal.module.css";
-
-import { WindupChildren } from "windups";
-import VisuallyHidden from "@reach/visually-hidden";
-
 import CharacterView from "../characterView";
-import WorldView from "../worldView";
 
 export default function GameWindow(): React.ReactElement {
 
-    // we duplicate it to display content differently visually and when accessing via screen reader
     return (
         <Tabs isFitted>
             <TabList>
-            <Tab>Settlements</Tab>
             <Tab>Characters</Tab>
             </TabList>
         
             <TabPanels>
-            <TabPanel>
-                <WorldView />
-            </TabPanel>
             <TabPanel>
                 <CharacterView />
             </TabPanel>
