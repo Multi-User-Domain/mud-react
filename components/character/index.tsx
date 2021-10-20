534cf9bd2ae1f340e3935d75ee9f895f2f87c5a2
Calum Mackervoy <c.mackervoy@gmail.com>
2021-10-20 19:49:34 +0200

    profile button in Character row

--- components/character/index.tsx
+++ components/character/index.tsx
@@ -1,13 +1,16 @@
-import { Grid, GridItem, Center, Text, Container } from "@chakra-ui/react";
+import { Grid, GridItem, Center, Text, Button, useDisclosure } from "@chakra-ui/react";
 import { getThingName } from '../../lib/utils';
 import ThingDepiction from "../thingDepiction";
 import { getUrl } from '@inrupt/solid-client';
 import { IRowComponent } from "../thingList";
+import CharacterProfile from '../characterProfile';
 import TimerProgressBar from "../timerProgressBar";
 import { MUD_CHARACTER } from "../../lib/MUD";
 import styles from "./character.module.css";
 
 export default function Character({thing, selectHandler} : IRowComponent): React.ReactElement {  
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  
   const onCharacterSelect = (event) => {
     selectHandler(thing);
   }
@@ -25,7 +28,7 @@ export default function Character({thing, selectHandler} : IRowComponent): React
   }
   
   return (
-  <Grid templateColumns="repeat(6, 1fr)" w="80%" margin="auto" gap={1} className={styles.characterRow}>
+  <Grid templateColumns="repeat(7, 1fr)" w="80%" margin="auto" gap={1} className={styles.characterRow}>
     <GridItem w="100px" h="100px" colSpan={2} className={styles.profilePic}
         tag="a" onClick={onCharacterSelect} style={{ cursor: "pointer" }}>
       <ThingDepiction thing={thing} />
@@ -38,6 +41,11 @@ export default function Character({thing, selectHandler} : IRowComponent): React
     
     <GridItem colSpan={2} className={styles.characterField}>
       <Center h="100%">{taskComponent}</Center>
+    </GridItem>
+
+    <GridItem colSpan={1} className={styles.characterField}>
+      <CharacterProfile character={thing} isOpen={isOpen} onClose={onClose} />
+      <Center><Button onClick={onOpen}>Profile</Button></Center>
     </GridItem>
   </Grid>);
   }
