83d7f050d04af9748ab37afc109bb84caaf6beed
Calum Mackervoy <c.mackervoy@gmail.com>
2021-11-07 12:40:21 +0100

    (not building) assorted rollup changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+//import pkg from "./package.json";
 import typescript from "@rollup/plugin-typescript";
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
@@ -7,7 +8,6 @@ import json from "@rollup/plugin-json";
 import css from 'rollup-plugin-css-only';
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 export default {
   input: "./index.tsx",
@@ -16,28 +16,31 @@ export default {
     nodeResolve({ 
         extensions: [".js", ".jsx", ".ts", ".tsx"],
         module: true,
-        preferBuiltins: false
+        preferBuiltins: false,
+        browser: true
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify( 'development' )
+      'process.env.NODE_ENV': JSON.stringify( 'development' ),
+      'preventAssignment': true
     }),
     babel({ presets: [
       ["@babel/preset-env", { targets: { node: "current" } }],
-      //["@babel/preset-react", {"runtime": "automatic"}],
       "@babel/preset-react",
       "@babel/preset-typescript",
       ],
-      plugins: ["@babel/plugin-syntax-dynamic-import"],
+      plugins: [],
       extensions: [".js", ".jsx", ".ts", ".tsx"],
-      exclude: /node_modules/
+      exclude: 'node_modules/**'
     }),
     commonjs({
         extensions: [".js", ".jsx", ".ts", ".tsx"],
         include: /node_modules/
     }),
-    typescript({ tsconfig: "./tsconfig.json" }),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      exclude: 'node_modules/**'
+    }),
     json(),
-    nodePolyfills( /* options */ ),
     serve({
         open: true,
         verbose: true,
@@ -47,29 +50,22 @@ export default {
     }),
     livereload({ watch: "dist" })
   ],
-  external: [],
+  external: [
+    '@inrupt/solid-client',
+    '@inrupt/solid-client-authn-core',
+  ],
   paths: {
     "react": "https://unpkg.com/react@17/umd/react.development.js",
-    "react-dom": "https://unpkg.com/react-dom@17/umd/react-dom.development.js"
+    "react-dom": "https://unpkg.com/react-dom@17/umd/react-dom.development.js",
+    "@inrupt/solid-client": "https://unpkg.com/@inrupt/solid-client@1.13.3/dist/index.js",
   },
   output: {
     globals: {
       'react': 'React',
       'react-dom': 'ReactDOM',
-      /*'events': 'events',
-      'buffer': 'buffer',
-      'process': 'process',
-      'crypto': 'crypto',
-      'stream': 'stream',
-      'util': 'util',
-      'http': 'http',
-      'url': 'url',
-      'https': 'https',
-      'zlib': 'zlib',
-      'assert': 'assert',
-      'tty': 'tty',
-      'os': 'os'*/
     },
+    // hack from a strange issue: https://github.com/rollup/rollup-plugin-commonjs/issues/6#issuecomment-519537010
+    intro: 'const global = window;',
     file: "dist/index.js",
     //dir: 'dist',
     format: "iife",
